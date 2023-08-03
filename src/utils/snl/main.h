e16c11ac6d69404b813bf8d7ee098ce9e29fd066
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-08-03 17:59:54 +0700

    Moved SnL functions' implementation to .cpp (i had accidentally put it to .h)

--- src/utils/snl/main.h
+++ src/utils/snl/main.h
@@ -1,38 +1,10 @@
 #include <iostream>
-#include <fstream>
-#include <iostream>
 using namespace std;
 
-#include "../logger/main.h"
-
 namespace snl {
-    // Creates file
-    void PatchFile(string path) {
-        ofstream file; file.open(path); file.close();
-    }
-    // Returns 0 if file is ok, 1 if file is not ok
-    int CheckFile(string path) {
-        ifstream file; file.open(path);
-        if (file) {file.close(); return 0;}
-        else {return 1;}
-    }
-    // Returns 0 if file is ok, 1 if file is not ok and logs it
-    int CheckLogFile(string path) {
-        int result = CheckFile(path);
-        if (result == 0) {Logger::Log(Logger::SnL_LevelOk, "File " + path + " is ok");}
-        else {Logger::Log(Logger::SnL_LevelFatal, "File " + path + " isn't ok");}
-        std::exit(1);
-        return result;
-    }
-    // Pathes file if it is not ok
-    void CheckPatchFile(string path) {
-        int result = CheckFile(path);
-        if (result != 0) {PatchFile(path);}
-    }
-    // Pathes file if it is not ok and logs it
-    void CheckPatchLogFile(string path) {
-        int result = CheckFile(path);
-        if (result == 0) {Logger::Log(Logger::SnL_LevelOk, "File " + path + " is ok");}
-        else {PatchFile(path); Logger::Log(Logger::SnL_LevelPatch, "File " + path + " isn't ok, patched");}
-    }
+    void PatchFile(string);
+    int CheckFile(string path);
+    int CheckLogFile(string path);
+    void CheckPatchFile(string path);
+    void CheckPatchLogFile(string path);
 }
